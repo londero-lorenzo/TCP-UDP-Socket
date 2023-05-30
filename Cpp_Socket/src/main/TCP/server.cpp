f268edfcf56a17a7ab9097cf122f8f61e74fe144
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-05-31 00:40:35 +0200

    implemented auto and manual initialization

--- Cpp_Socket/src/main/TCP/server.cpp
+++ Cpp_Socket/src/main/TCP/server.cpp
@@ -4,13 +4,24 @@
 
 // viene incluso il file header contenente le dichiarazioni del Client TCP
 #include "../../TCP/Server.h"
+#include "../../utils/Arguments.cpp"
 
 
 // viene definito il blocco principale del programma lato Client TCP
-int main() {
-
-    // viene impostata la porta alla viene aperto il server
-    int port = 5500;
+int main(int argc, char *argv[]) {
+    int port;
+    if (argc == 1) {
+        // viene impostata la porta alla viene aperto il server
+        port = 5500;
+    } else {
+        int argument = getArgumentType(argv[2]);
+        if (argument == PORT_ARG || argument == DATA_ARG)
+            port = getPortFromArgument(argv[2]);
+    }
+    if (port < 0)
+        printErrorWithExit(new char[]{
+                "Errore durante l'inizializzazione delle variabili per la connessione al server!"
+        });
 
     // viene creato l'oggetto server
     Socket::TCP::Server server;
