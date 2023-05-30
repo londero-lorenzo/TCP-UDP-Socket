ae5e90f814819a52eeba730a39a69725041831fe
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-05-31 00:38:43 +0200

    functions implemented to print errors more efficiently

---
+++ Cpp_Socket/src/utils/Error.cpp
@@ -0,0 +1,39 @@
+//
+// Created by londe on 30/05/2023.
+//
+#include <Winsock2.h>
+#include <iostream>
+
+/**
+ * Funzione utilizzata per stampare a schermo un errore generico.
+ * @param description Descrizione dell'errore
+ */
+static void printError(char *description) {
+    std::cerr << description << std::endl;
+}
+
+static void printErrorWithExit(char *description)
+{
+    std::cerr << description << std::endl;
+    exit(-1);
+}
+
+/**
+ * Metodo utilizzato per stampare a schermo gli eventuali errori generati durante l'esecuzione del programma.
+ * @param description Descrizione dell'errore
+ */
+static void printWSAError(char *description) {
+    printError(description);
+    std::cerr << "Codice d'uscia: " << WSAGetLastError() << std::endl;
+    WSACleanup();
+}
+
+/**
+ * Metodo alternativo utilizzato per stampare a schermo gli eventuali errori generati durante l'esecuzione del programma prima del caricamento di WSAStartup.
+ * @param description Descrizione dell'errore
+ * @param exitCode Codice d'uscita nel momento del caricamento di WSAStartup
+ */
+static void printErrorBeforeWSAInit(char *description, int exitCode) {
+    std::cerr << description << std::endl;
+    std::cerr << "Codice d'uscia: " << exitCode << std::endl;
+}
