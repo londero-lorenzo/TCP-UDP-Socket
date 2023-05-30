f549a20734fb2c67ea7705b208c94a1704de00e6
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-05-31 00:37:12 +0200

    renamed Client variable definition, comments added

--- Cpp_Socket/src/TCP/Client.h
+++ Cpp_Socket/src/TCP/Client.h
@@ -2,53 +2,72 @@
 // Created by Londero Lorenzo on ~ April 2023.
 //
 
+// viene verificato che il file header per il client non sia già stato definito
+#ifndef SOCKET_TCP_CLIENT_H
 
-#ifndef CPP_SOCKET_CLIENT_H
-#define CPP_SOCKET_CLIENT_H
+// viene definito l'identificatore per questo file header associato all'oggetto a cui fa riferimento
+#define SOCKET_TCP_CLIENT_H
 
+// vengono incluse le librerie utili per il funzionamento del socket
 #include <Winsock2.h>
 #include <iostream>
 
-namespace Socket::TCP {
+// viene aggiunta la libreria "ws2_32.lib" alla lista delle librerie utili per il funzionamento del socket
+#pragma comment(lib, "ws2_32.lib")
 
+// viene definito lo spazio logico nel quale si troverà l'oggetto di seguito definito
+/**
+ * Spazio logico nel quale sono racchiusi tutti gli oggetti Socket
+ */
+namespace Socket::TCP {
+    /**
+     * Classe Client utilizzata per le comunicazioni TCP.
+     */
     class Client {
+    // vengono dichiarate le variabili private associate a questo oggetto
     private:
         // struttura contenente le informazioni per l'implementazione dei Socket in Windows
         WSAData wsaData{};
 
+        // variabile tipo SOCKET utilizzata per avere un riferimento alla struttura Socket
         SOCKET sock;
 
-        // struttura contenente varie informazioni per il funzionamento del socket
+        // struttura destinata per ospitare le informazioni relative all'indirizzo del destinatario
         sockaddr_in destinationAddr{};
 
+        // struttura destinata per ospitare le informazioni relative all'indirizzo del client stesso
         sockaddr_in clientAddr{};
 
+        // valore intero relativo alla grandezza della struttura dati contenente l'indirizzo del client stesso
         int clientAddrLen = sizeof(clientAddr);
 
         // buffer di caratteri utilizzato per essere riempito con i dati in ricezione
         char receivingBuffer[1024]{};
 
-
+    // vengono dichiarate le variabili pubbliche associate a questo oggetto
     public:
-        // viene dichiarato un costruttore pubblico
+        // viene dichiarato il costruttore pubblico
         Client();
 
-        explicit Client(SOCKET *serverSock);
+        // viene dichiarato un costruttore pubblico alternativo
+        explicit Client(const SOCKET *serverSock);
 
-        // viene dichiarato un metodo void per permettere la connessione al server
+        // viene dichiarato il metodo void per permettere la connessione al server
         void connectTo(char *ip, int port);
 
-        // viene dichiarato un metodo void per permettere d'inviare dati al server
+        // viene dichiarato il metodo void per permettere d'inviare dati al server
         void send(char *data) const;
 
-        // viene dichiarato un metodo void per permettere la ricezione dei dati dal server
-        char* receive();
+        // viene dichiarato il metodo void per permettere la ricezione dei dati dal server
+        char *receive();
 
-        char* getIp();
+        // viene dichiarato il metodo per ottenere l'indirizzo ip di questo socket
+        [[nodiscard]] char *getIp();
 
+        // viene dichiarato il metodo per chiudere il socket
         void close() const;
     };
 
-} // Socket
+} // viene chiuso lo spazio logico nel quale è presente questo oggetto
 
-#endif //CPP_SOCKET_CLIENT_H
+#endif //SOCKET_TCP_CLIENT_H
