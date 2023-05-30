180ea41cb186e751a70a7f741e33a877d7baf445
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-05-31 00:37:45 +0200

    renamed Client variable definition, comments added

--- Cpp_Socket/src/UDP/Client.h
+++ Cpp_Socket/src/UDP/Client.h
@@ -2,63 +2,81 @@
 // Created by Londero Lorenzo on ~ April 2023.
 //
 
+// viene verificato che il file header per il client non sia già stato definito
+#ifndef SOCKET_UDP_CLIENT_H
 
-#ifndef CPP_SOCKET_CLIENT_H
-#define CPP_SOCKET_CLIENT_H
+// viene definito l'identificatore per questo file header associato all'oggetto a cui fa riferimento
+#define SOCKET_UDP_CLIENT_H
 
+// vengono definiti degli identificatori utili per riconoscere il funzionamento del socket
+#define CLIENT 0 // identificativo utilizzato per indicare che il socket funziona dal lato client
+#define SERVER 1 // identificativo utilizzato per indicare che il socket funziona del lato server
 
-#define CLIENT 0
-#define SERVER 1
-
+// vengono incluse le librerie utili per il funzionamento del socket
 #include <Winsock2.h>
 #include <iostream>
-
+// viene aggiunta la libreria "ws2_32.lib" alla lista delle librerie utili per il funzionamento del socket
 #pragma comment(lib, "ws2_32.lib")
 
+// viene definito lo spazio logico nel quale si troverà l'oggetto di seguito definito
+/**
+ * Spazio logico nel quale sono racchiusi tutti gli oggetti Socket
+ */
 namespace Socket::UDP {
-
+    /**
+     * Classe Client utilizzata per le comunicazioni UDP.
+     */
     class Client {
+    // vengono dichiarate le variabili private associate a questo oggetto
     private:
         // struttura contenente le informazioni per l'implementazione dei Socket in Windows
         WSAData wsaData{};
 
+        // variabile tipo SOCKET utilizzata per avere un riferimento alla struttura Socket
         SOCKET sock;
 
-        // struttura contenente varie informazioni per il funzionamento del socket
+        // struttura destinata per ospitare le informazioni relative all'indirizzo del destinatario
         sockaddr_in destinationAddr{};
 
+        // valore intero relativo alla grandezza della struttura dati contenente l'indirizzo del destinatario
         int destinationAddrLen = sizeof(destinationAddr);
 
+        // struttura destinata per ospitare le informazioni relative all'indirizzo del client stesso
         sockaddr_in clientAddr{};
 
         // buffer di caratteri utilizzato per essere riempito con i dati in ricezione
         char receivingBuffer[1024]{};
 
+        // valore intero utilizzato per indicare su quale lato opera questo socket
         int side;
 
-
+    // vengono dichiarate le variabili pubbliche associate a questo oggetto
     public:
-        // viene dichiarato un costruttore pubblico
+        // viene dichiarato il costruttore pubblico
         Client();
 
+        // viene dichiarato un costruttore pubblico alternativo
         explicit Client(const SOCKET *clientSock);
 
-        // viene dichiarato un metodo void per permettere la connessione al server
+        // viene dichiarato il metodo void per permettere la connessione al server
         void connectTo(char *ip, int port);
 
-        // viene dichiarato un metodo void per permettere d'inviare dati al server
+        // viene dichiarato il metodo void per permettere d'inviare dati al server
         void send(char *data);
 
-        // viene dichiarato un metodo void per permettere la ricezione dei dati dal server
+        // viene dichiarato il metodo void per permettere la ricezione dei dati dal server
         char *receive();
 
+        // viene dichiarato il metodo per ottenere l'indirizzo ip di questo socket
         char *getIp();
 
+        // viene dichiarato il metodo per ottenere la porta di questo socket
         int getPort() const;
 
+        // viene dichiarato il metodo per chiudere il socket
         void close() const;
     };
 
-} // Socket
+} // viene chiuso lo spazio logico nel quale è presente questo oggetto
 
-#endif //CPP_SOCKET_CLIENT_H
+#endif  //SOCKET_UDP_CLIENT_H
