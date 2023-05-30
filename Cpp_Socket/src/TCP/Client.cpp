84222f368346d910bcf9c7090ed67a6bb4883e51
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-05-31 00:34:41 +0200

    implemented more efficient error output

--- Cpp_Socket/src/TCP/Client.cpp
+++ Cpp_Socket/src/TCP/Client.cpp
@@ -2,24 +2,30 @@
 // Created by Londero Lorenzo on ~ April 2023.
 //
 
-// vengono incluse le librerie utili per il funzionamneto del socket
+// vengono incluse le librerie utili per il funzionamento del socket
 #include "Client.h"
 #include <Winsock2.h>
 #include <ws2ipdef.h>
 #include <WS2tcpip.h>
+#include <string>
+#include "../utils/Error.cpp"
+
 #pragma comment(lib, "ws2_32.lib")
 
+// viene definito lo spazio logico nel quale si troverà l'oggetto al suo interno
 namespace Socket::TCP {
 
     /**
-     * Costruttore pubblico generico, si occupa dell'inizializzazione dell'oggetto socket
+     * Costruttore pubblico generico, si occupa dell'inizializzazione dell'oggetto socket.
      */
     Client::Client() {
         // viene caricata la struttura contenente le informazioni per l'implementazione dei Socket in Windows
         int result = WSAStartup(MAKEWORD(2, 2), &(Client::wsaData));
         // viene controllato il risultato del caricamento
         if (result != 0) {
-            std::cerr << "Errore durante il caricamento di WSAStartup: " << result << std::endl;
+            printErrorBeforeWSAInit(new char[]{
+                    "Errore durante il caricamento di WSAStartup."
+            }, result);
             return;
         }
         // AF_INET = famiglia dell'indirizzo utilizzato per il socket (IPv4)
@@ -28,29 +34,33 @@ namespace Socket::TCP {
         Client::sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         // viene verificato che l'oggetto socket creato sia valido
         if (Client::sock == INVALID_SOCKET) {
-            std::cerr << "Socket invalido: " << WSAGetLastError() << std::endl;
-            WSACleanup();
+            printWSAError(new char[]{
+                    "Errore durante il caricamento del socket."
+            });
             return;
         }
     }
 
     /**
-     * Costruttore pubblico alternativo, utilizzato dalla parte server una volta che viene captato un tentativo di connessione
+     * Costruttore pubblico alternativo, utilizzato dalla parte server una volta che viene captato un tentativo di connessione.
      * @param serverSock
      */
-    Client::Client(SOCKET *serverSock) {
+    Client::Client(const SOCKET *serverSock) {
         int result = WSAStartup(MAKEWORD(2, 2), &(Client::wsaData));
         // viene controllato il risultato del caricamento
         if (result != 0) {
-            std::cerr << "Errore durante il caricamento di WSAStartup: " << result << std::endl;
+            printErrorBeforeWSAInit(new char[]{
+                    "Errore durante il caricamento di WSAStartup."
+            }, result);
             return;
         }
 
-        SOCKET clientSock = accept((SOCKET) serverSock, (struct sockaddr *) &(Client::clientAddr),
+        SOCKET clientSock = accept(*serverSock, (struct sockaddr *) &(Client::clientAddr),
                                    &(Client::clientAddrLen));
-        if (clientSock == INVALID_SOCKET)
-        {
-            std::cerr << "Errore durante la creazione del socket client: " << WSAGetLastError() << std::endl;
+        if (clientSock == INVALID_SOCKET) {
+            printWSAError(new char[]{
+                    "Errore durante il caricamento del socket."
+            });
             return;
         }
 
@@ -62,7 +72,7 @@ namespace Socket::TCP {
 
     /**
      * Metodo utilizzato per permettere la connessione al server.<br>
-     * Perché un socket si possa connettere ad un server, è necessario fornire l'indirizzo ip e la porta sulla quale sta funzionando il servizio TCP
+     * Perché un socket si possa connettere ad un server, è necessario fornire l'indirizzo ip e la porta sulla quale sta funzionando il servizio TCP.
      * @param ip Indirizzo ip del server al quale ci si vuole connettere
      * @param port Porta del server al quale ci si vuole connettere
      */
@@ -83,10 +93,12 @@ namespace Socket::TCP {
                              sizeof(Client::destinationAddr));
         // viene verificato che non siano insorti errori nel momento della connessione
         if (result == SOCKET_ERROR) {
-            std::cerr << "Errore durante l'inizializzazione della connessione verso \"" << ip << "\" alla porta "
-                      << port << std::endl << WSAGetLastError() << std::endl;
+            std::string outputStringError =
+                    "\nErrore durante l'inizializzazione della connessione verso: " + std::string(ip) + ":" +
+                    std::to_string(port);
+
+            printWSAError(outputStringError.data());
             closesocket(Client::sock);
-            WSACleanup();
             return;
         }
 
@@ -103,9 +115,10 @@ namespace Socket::TCP {
         int result = ::send(Client::sock, data, (int) strlen(data), 0);
         // viene verificato che non siano insorti errori durante l'invio dei dati
         if (result == SOCKET_ERROR) {
-            std::cerr << "Errore durante l'invio dei dati" << std::endl;
+            printWSAError(new char[]{
+                    "Errore durante l'invio dei dati."
+            });
             closesocket(Client::sock);
-            WSACleanup();
             return;
         } else
             std::cout << "Byte inviati: " << result << std::endl;
@@ -122,9 +135,10 @@ namespace Socket::TCP {
         int result = recv(Client::sock, Client::receivingBuffer, sizeof(Client::receivingBuffer), 0);
         // viene verificato che non siano insorti errori durante la ricezione dei dati
         if (result == SOCKET_ERROR) {
-            std::cerr << "Errore durante la ricezione dei dati" << std::endl << WSAGetLastError();
+            printWSAError(new char[]{
+                    "Errore durante la ricezione dei dati."
+            });
             closesocket(Client::sock);
-            WSACleanup();
             return nullptr;
         } else {
             std::cout << "Byte ricevuti: " << result << std::endl;
@@ -134,12 +148,19 @@ namespace Socket::TCP {
         return Client::receivingBuffer;
     }
 
+    /**
+     * Metodo utilizzato per ottenere l'indirizzo ip del client
+     * @return Sequenza di caratteri che identificano l'indirizzo ip del client
+     */
     char *Client::getIp() {
         char *str = (char *) malloc(sizeof(char) * INET_ADDRSTRLEN);
         InetNtop(AF_INET, &(Client::clientAddr.sin_addr), str, INET_ADDRSTRLEN);
         return str;
     }
 
+    /**
+     * Metodo utilizzato per chiudere il socket.
+     */
     void Client::close() const {
         closesocket(Client::sock);
     }
