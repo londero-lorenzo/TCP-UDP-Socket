a2e6858836c41aa7b5a72cfa29236ef5d4988ce3
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2023-05-31 17:06:19 +0200

    comments fixed

--- Cpp_Socket/src/main/TCP/client.cpp
+++ Cpp_Socket/src/main/TCP/client.cpp
@@ -4,34 +4,49 @@
 
 // viene incluso il file header contenente le dichiarazioni del Client TCP
 #include "../../TCP/Client.h"
+// viene incluso il file cpp contenente le funzioni per elaborare gli argomenti passati al programma in fase d'avvio
 #include "../../utils/Arguments.cpp"
 
 
-// viene definito il blocco principale del programma lato Client TCP
+/**
+ * Blocco principale per l'esecuzione del programma lato Client TCP.
+ * @param argc Numero di argomenti passati all'avvio dell'applicazione
+ * @param argv Stringhe di argomenti passati all'avvio dell'applicazione
+ */
 int main(int argc, char *argv[]) {
-    char *ip, *stringToSend;
-    int port;
+    char *ip = nullptr, *stringToSend = nullptr;
+    int port = -1;
+
+    /*
+    * ---------Inizializzazione variabili utili per il funzionamento del server---------
+    */
+    // viene controllato il numero di argomenti passati al programma
     if (argc == 1) {
-        // viene dichiarato e definito l'indirizzo IP del server al quale ci si vuole connettere
+        //(la prima posizione di argv contiene il nome del programma, quindi è scartabile)
+        // viene utilizzato l'indirizzo ip di default
         ip = new char[]{
                 "192.168.178.134"
         };
-        // viene impostata la porta alla quale connettersi
+        // viene utilizzata la porta di default
         port = 5500;
 
-        // viene dichiarato e definito il messaggio da inviare al server
+        // viene utilizzata una stringa di default da inviare
         stringToSend = new char[]{
                 "Ciao mondo!"
         };
     } else
+        // per ogni argomento passato al programma, vengono inizializzate le variabili utili per la connessione al server
         for (int i = 0; i < argc; i++)
             setNetworkValuesFromArgument(&argv[i], &ip, &port, &stringToSend);
+    // se almeno una delle variabili utili per instaurare una connessione con il server non viene inizializzata, viene lanciato un messaggio d'errore
     if (ip == nullptr || stringToSend == nullptr || port < 0)
         printErrorWithExit(new char[]{
                 "Errore durante l'inizializzazione delle variabili per la connessione al server!"
         });
 
-
+    /*
+     * ---------Istruzioni per la comunicazione con il server---------
+     */
     // viene creato l'oggetto client
     Socket::TCP::Client client;
 
--- Cpp_Socket/src/main/TCP/server.cpp
+++ Cpp_Socket/src/main/TCP/server.cpp
@@ -4,29 +4,45 @@
 
 // viene incluso il file header contenente le dichiarazioni del Client TCP
 #include "../../TCP/Server.h"
+// viene incluso il file cpp contenente le funzioni per elaborare gli argomenti passati al programma in fase d'avvio
 #include "../../utils/Arguments.cpp"
 
 
-// viene definito il blocco principale del programma lato Client TCP
+/**
+ * Blocco principale per l'esecuzione del programma lato Server TCP.
+ * @param argc Numero di argomenti passati all'avvio dell'applicazione
+ * @param argv Stringhe di argomenti passati all'avvio dell'applicazione
+ */
 int main(int argc, char *argv[]) {
-    int port;
+    int port = -1;
+
+    /*
+     * ---------Inizializzazione variabili utili per il funzionamento del server---------
+     */
+    // viene controllato il numero di argomenti passati al programma
     if (argc == 1) {
-        // viene impostata la porta alla viene aperto il server
+        // (la prima posizione di argv contiene il nome del programma, quindi è scartabile)
+        // viene utilizzata la porta di default
         port = 5500;
     } else {
-        int argument = getArgumentType(argv[2]);
+        // viene controllato che l'argomento passato corrisponda alla porta a cui aprire il server
+        int argument = getArgumentType(argv[1]);
         if (argument == PORT_ARG || argument == DATA_ARG)
-            port = getPortFromArgument(argv[2]);
+            port = getPortFromArgument(argv[1]);
     }
+    // se la porta non è stata inizializzata, viene rilasciato un messaggio d'errore
     if (port < 0)
         printErrorWithExit(new char[]{
                 "Errore durante l'inizializzazione delle variabili per la connessione al server!"
         });
 
+    /*
+     * ---------Istruzioni per l'avvio del server---------
+     */
     // viene creato l'oggetto server
     Socket::TCP::Server server;
 
-    // viene avviato il servizio del server
+    // viene avviato il servizio del server alla porta indicata
     server.start(port);
 
     // viene chiuso il socket server
